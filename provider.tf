42ba77732306140114c09021d01ce3660785355a
vchevychelov <vchevychelov@ya.ru>
2023-01-03 07:11:29 +0300

    edit file:   provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,6 @@
-provider_installation {
-   dev_overrides {
-    "yandex-cloud/yandex" = "./terraform-provider-yandex_v0.84.0"
-  }
-
-   direct {}
- }
+provider "yandex" {
+  token     = "y0_AgAAAABnXmIqAATuwQAAAADXy1FTRNMoi3GvREGW3UmJRHw9t7fOLUo"
+  cloud_id  = "myyacloud"
+  folder_id = "b1g1d9s6l3sl8friluq4"
+  zone      = "ru-central1-a"
+}
