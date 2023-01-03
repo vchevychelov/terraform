1732529cb2d5963498480da64679801eab8d8210
vchevychelov <vchevychelov@ya.ru>
2023-01-03 07:17:27 +0300

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -6,13 +6,6 @@ terraform {
   }
 }
 
-provider "yandex" {
-  token     = "y0_AgAAAABnXmIqAATuwQAAAADXy1FTRNMoi3GvREGW3UmJRHw9t7fOLUo"
-  cloud_id  = "myyacloud"
-  folder_id = "b1g1d9s6l3sl8friluq4"
-  zone      = "ru-central1-a"
-}
-
 resource "yandex_compute_instance" "vm-1" {
   name = "dev"
 
